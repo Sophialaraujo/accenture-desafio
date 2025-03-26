36729ba602d348e8d297017f5a14435008b38c22
Sophialaraujo <sophia.laraujo@hotmail.com>
2025-03-25 21:42:04 -0300

    Refatorando primeiro teste.

---
+++ desafio-2-frontend/cypress/e2e/POM/practice_form.js
@@ -0,0 +1,126 @@
+import 'cypress-file-upload';
+
+export class PracticeForm {
+  // Exemplo de getters para cada elemento
+  firstNameField() {
+    return cy.get('#firstName');
+  }
+
+  lastNameField() {
+    return cy.get('#lastName');
+  }
+
+  userEmailField() {
+    return cy.get('#userEmail');
+  }
+
+  userNumberField() {
+    return cy.get('#userNumber');
+  }
+
+  currentAddressField() {
+    return cy.get('#currentAddress');
+  }
+
+  dateOfBirthField() {
+    return cy.get('#dateOfBirthInput');
+  }
+
+  subjectsField() {
+    return cy.get('.subjects-auto-complete__value-container');
+  }
+
+  uploadFileButton() {
+    return cy.get('#uploadPicture');
+  }
+
+  stateField() {
+    return cy.get('#state > .css-yk16xz-control');
+  }
+
+  cityField() {
+    return cy.get('#city > .css-yk16xz-control > .css-1hwfws3');
+  }
+
+  submitButton() {
+    return cy.get('#submit');
+  }
+
+  // Listas de checkboxes
+  genderCheckBoxList() {
+    return [
+      cy.get('#genterWrapper > .col-md-9 > :nth-child(1)'),
+      cy.get('#genterWrapper > .col-md-9 > :nth-child(2)'),
+      cy.get('#genterWrapper > .col-md-9 > :nth-child(3)'),
+    ];
+  }
+
+  hobbiesCheckBoxList() {
+    return [
+      cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label'),
+      cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(2) > .custom-control-label'),
+      cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(3) > .custom-control-label'),
+    ];
+  }
+
+  // Métodos de ação
+
+  setFirstNameField(firstName) {
+    this.firstNameField().type(firstName);
+  }
+
+  setLastNameField(lastName) {
+    this.lastNameField().type(lastName);
+  }
+
+  setUserEmailField(userEmail) {
+    this.userEmailField().type(userEmail);
+  }
+
+  setUserNumberField(userNumber) {
+    this.userNumberField().type(userNumber);
+  }
+
+  setCurrentAddressField(address) {
+    this.currentAddressField().type(address);
+  }
+
+  setGenderCheckbox(genderOption) {
+    // genderOption deve ser 0, 1 ou 2
+    this.genderCheckBoxList()[genderOption].click();
+  }
+
+  setDateOfBirth() {
+    this.dateOfBirthField().click();
+    cy.get(':nth-child(3) > .react-datepicker__day--012').click();
+  }
+
+  setSubjectsField() {
+    this.subjectsField().type('Account{enter}');
+    this.subjectsField().type('Math{enter}');
+  }
+
+  setHobbiesCheckbox(hobbyOption) {
+    this.hobbiesCheckBoxList()[hobbyOption].click();
+  }
+
+  uploadPicture(filePath) {
+    this.uploadFileButton().attachFile(filePath);
+  }
+
+  setStateField() {
+    this.stateField().type('a{enter}');
+  }
+
+  setCityField() {
+    this.cityField().type('a{enter}');
+  }
+
+  submitForm() {
+    this.submitButton().click();
+  }
+
+  closeModal() {
+    cy.get('body').type('{esc}');
+  }
+}
