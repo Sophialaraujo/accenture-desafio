21214b6506f0c9917a4184dcd0f4f014771e4197
Sophialaraujo <sophia.laraujo@hotmail.com>
2025-03-25 21:57:41 -0300

    Finalizando segundo teste.

--- desafio-2-frontend/cypress/e2e/e2e/mytest.cy.js
+++ desafio-2-frontend/cypress/e2e/e2e/mytest.cy.js
@@ -3,59 +3,72 @@ import 'cypress-file-upload';
 import { PracticeForm } from '../POM/practice_form';
 
 function gerarNumeroAleatorioDe10Digitos() {
-  const min = 1000000000;
-  const max = 9999999999;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    const min = 1000000000;
+    const max = 9999999999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function accessPracticeFormPage() {
-  cy.visit("https://demoqa.com/");
-  cy.get('.category-cards > :nth-child(2)').click();
-  cy.get(':nth-child(2) > .element-list > .menu-list > #item-0').click();
+    cy.visit("https://demoqa.com/");
+    cy.get('.category-cards > :nth-child(2)').click();
+    cy.get(':nth-child(2) > .element-list > .menu-list > #item-0').click();
 }
 
-describe('suite de teste', () => {
-  const firstName = fakerBr.name.firstName();
-  const lastName = fakerBr.name.lastName();
-  const userEmail = fakerBr.internet.email();
-  const genderOption = Math.floor(Math.random() * 3);  
-  const hobbiesOption = Math.floor(Math.random() * 3);
-  const userNumber = gerarNumeroAleatorioDe10Digitos();
-  const address = fakerBr.address.streetAddress();
-
-  it('primeiro teste', () => {
-    accessPracticeFormPage();
-
-    const practiceForm = new PracticeForm();
-
-    practiceForm.setFirstNameField(firstName);
-    practiceForm.setLastNameField(lastName);
-    practiceForm.setUserEmailField(userEmail);
-
-    practiceForm.setGenderCheckbox(genderOption);
-
-    practiceForm.setUserNumberField(userNumber);
-    practiceForm.setDateOfBirth();
-    practiceForm.setSubjectsField();
-
-    practiceForm.setHobbiesCheckbox(hobbiesOption);
-
-    practiceForm.setCurrentAddressField(address);
-    
-    practiceForm.uploadPicture('file.txt');
-    
-    practiceForm.setStateField();
-    practiceForm.setCityField();
-
-    practiceForm.submitForm();
-
-    practiceForm.closeModal();
-  });
-
-  it('segundo teste', () => {
+function accessBrowserWindowsPage() {
     cy.visit("https://demoqa.com/");
     cy.get('.category-cards > :nth-child(3)').click({ force: true });
     cy.get(':nth-child(3) > .element-list > .menu-list > #item-0').click();
-    cy.get('#windowButton').click();
-  });
+}
+
+describe('Demoqa Tests', () => {
+    const firstName = fakerBr.name.firstName();
+    const lastName = fakerBr.name.lastName();
+    const userEmail = fakerBr.internet.email();
+    const genderOption = Math.floor(Math.random() * 3);
+    const hobbiesOption = Math.floor(Math.random() * 3);
+    const userNumber = gerarNumeroAleatorioDe10Digitos();
+    const address = fakerBr.address.streetAddress();
+
+    it('Practice Form Test', () => {
+        accessPracticeFormPage();
+
+        const practiceForm = new PracticeForm();
+
+        practiceForm.setFirstNameField(firstName);
+        practiceForm.setLastNameField(lastName);
+        practiceForm.setUserEmailField(userEmail);
+
+        practiceForm.setGenderCheckbox(genderOption);
+
+        practiceForm.setUserNumberField(userNumber);
+        practiceForm.setDateOfBirth();
+        practiceForm.setSubjectsField();
+
+        practiceForm.setHobbiesCheckbox(hobbiesOption);
+
+        practiceForm.setCurrentAddressField(address);
+
+        practiceForm.uploadPicture('file.txt');
+
+        practiceForm.setStateField();
+        practiceForm.setCityField();
+
+        practiceForm.submitForm();
+
+        practiceForm.closeModal();
+    });
+
+    it('Browser Windows Test', () => {
+        accessBrowserWindowsPage();
+        
+        cy.window().then((win) => {
+            cy.stub(win, 'open').callsFake((url) => {
+                win.location.href = url;
+            }).as('windowOpen');
+        });
+
+        cy.get('#windowButton').click();
+
+        cy.contains('This is a sample page').should('be.visible');
+    });
 });
